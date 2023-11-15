b530073681211aef954393586150efda0cc3a86d
GiovaniGama <giovanigama.08@hotmail.com>
2023-11-15 14:02:24 -0300

    create project structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { PagesComponent } from './components/pages/pages.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    FooterComponent,
+    PagesComponent
   ],
   imports: [
     BrowserModule,

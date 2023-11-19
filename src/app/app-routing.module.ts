2b1c2f2a9f222829057a47a6e696e283fe4cd740
GiovaniGama <giovanigama.08@hotmail.com>
2023-11-19 13:31:10 -0300

    create components in my project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { AboutMeComponent } from './pages/about-me/about-me.component';
+import { ExperienceComponent } from './pages/experience/experience.component';
+import { MyProjectsComponent } from './pages/my-projects/my-projects.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
 const routes: Routes = [
   {path: '', redirectTo: '/home', pathMatch: 'full'},
-  {path: "home", component: HomeComponent}
+  {path: "home", component: HomeComponent},
+  {path: "sobre", component: AboutMeComponent},
+  {path: "experiencia", component: ExperienceComponent},
+  {path: "projetos", component: MyProjectsComponent},
+  {path: "contato", component: ContactComponent}
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes),
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,21 @@ import { AppComponent } from './app.component';
 import { MenuComponent } from './components/menu/menu.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HomeComponent } from './pages/home/home.component';
+import { AboutMeComponent } from './pages/about-me/about-me.component';
+import { ExperienceComponent } from './pages/experience/experience.component';
+import { MyProjectsComponent } from './pages/my-projects/my-projects.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
     FooterComponent,
-    HomeComponent
+    HomeComponent,
+    AboutMeComponent,
+    ExperienceComponent,
+    MyProjectsComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
